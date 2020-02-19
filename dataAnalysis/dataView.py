4ae70658660416e68194f933e6e1aed89729072b
liulebin <liulebin@58.com>
2020-02-20 01:51:41 +0800

    机器学习基础

--- dataAnalysis/dataView.py
+++ dataAnalysis/dataView.py
@@ -1,12 +1,9 @@
-<!--# import numpy as np-->
-<!--# import pandas as pd-->
-<!--# import matplotlib as mpl-->
-<!--# import matplotlib.pyplot as plt-->
-<!--#-->
-<!--# import seaborn as sns-->
-<!--# sns.set(color_codes=True)-->
-<!--#-->
-<!--# np.random.seed(sum(map(ord, "regression")))-->
-<!--#-->
-<!--# tips = sns.load_dataset("tips")-->
-<!--# print(tips.head())-->
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(color_codes=True)
+np.random.seed(sum(map(ord, "regression")))
+tips = sns.load_dataset("tips")
+print(tips.head())
